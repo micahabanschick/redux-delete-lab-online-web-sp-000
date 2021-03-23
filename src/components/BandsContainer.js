0444a01df283356623e7f64ef05a0dfe3bb3d0e3
Micah Banschick <micha.ban@gmail.com>
2021-03-23 19:40:14 +0000

    Automatically backed up by Learn

--- src/components/BandsContainer.js
+++ src/components/BandsContainer.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import BandInput from './BandInput';
-
-import { connect } from 'react-redux'
+import Bands from './Bands';
+import { connect } from 'react-redux';
 
 class BandsContainer extends Component {
   render() {
     return (
       <div>
         <BandInput addBand={this.props.addBand}/>
-
+        <Bands bands={this.props.bands} deleteBand={this.props.deleteBand} />
       </div>
     )
   }
